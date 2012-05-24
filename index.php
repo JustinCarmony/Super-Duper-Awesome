487c31bcb8d94b543a0d34acc8ab08a08a05b2aa
Justin Carmony <justin@justincarmony.com>
2012-05-23 23:54:26 -0500

    AWESOMENESS EVEN MORE

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1>AWWWEEEEESOME!!!!</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SUPER DUMER AWESOMENESS!!!!! WITH KITTIESSS!!!!!</title>
+</head>
+<body style="background:url(img/bg.gif);">
+    <h1>AWESOMENESS!!!!!</h1>
+</body>
+
+</html>
+
